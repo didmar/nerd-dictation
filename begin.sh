707bf04a5e8b2c1d8bc22229a95076073434e160
Didier Marin <mail@didiermarin.com>
2025-07-16 11:49:35 +0200

    integrate OpenAI Whisper API

--- begin.sh
+++ begin.sh
@@ -1,2 +1,5 @@
-# DOTOOL_XKB_LAYOUT=fr ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --vosk-model-dir model --speech-engine=ASSEMBLYAI &
- DOTOOL_XKB_LAYOUT=fr ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=FASTER_WHISPER &
+#!/bin/bash
+export DOTOOL_XKB_LAYOUT=fr
+# ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=OPENAI_WHISPER --openai-whisper-language en &
+./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=ASSEMBLYAI &
+# ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=FASTER_WHISPER --faster-whisper-model-size=medium &
