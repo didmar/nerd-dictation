5567481cb9d488a7385648c579b038e14b4c9432
Didier Marin <mail@didiermarin.com>
2025-07-17 12:19:44 +0200

    add voxtral

--- begin.sh
+++ begin.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export DOTOOL_XKB_LAYOUT=fr
 # ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=OPENAI_WHISPER --openai-whisper-language en &
-./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=ASSEMBLYAI &
+# ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=ASSEMBLYAI &
+./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=VOXTRAL &
 # ./venv/bin/python3 ./nerd-dictation begin --simulate-input-tool DOTOOL --speech-engine=FASTER_WHISPER --faster-whisper-model-size=medium &
